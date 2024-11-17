f2e17e27acbda20f7966241dfb4ea2fc5c1a9e3d
minerkg <129666850+minerkg@users.noreply.github.com>
2024-11-17 15:59:56 +0200

    working login tests, both working

--- src/test/java/org/csiszer/features/login/LoginStory.java
+++ src/test/java/org/csiszer/features/login/LoginStory.java
@@ -32,7 +32,7 @@ public class LoginStory {
     @Steps
     public EndUserSteps endUser;
 
-    @Issue("#WIKI-1")
+    @Issue("#Login")
     @Test
     public void loggingin_with_valid_data_should_proceed_to_home_page() {
         endUser.is_the_home_page();
@@ -45,11 +45,11 @@ public class LoginStory {
 
     }
 
-//    @Test
-//    public void loggingin_with_invalid_data_should_display_error_message() {
-//        endUser.is_the_home_page();
-//        endUser.logging_in_with_credentials(userName, password);
-//        endUser.should_see_an_error_message();
-//    }
+    @Test
+    public void loggingin_with_invalid_data_should_display_error_message() {
+        endUser.is_the_home_page();
+        endUser.logging_in_with_credentials(userName, password);
+        endUser.should_see_an_error_message();
+    }
 
 }
