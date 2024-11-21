7599b0ab28f03de19cf02df36a26295b840c2160
minerkg <129666850+minerkg@users.noreply.github.com>
2024-11-21 20:45:45 +0200

    unnecesarry code removed

--- src/test/java/org/csiszer/features/searchForProduct/SearchForProductStory.java
+++ src/test/java/org/csiszer/features/searchForProduct/SearchForProductStory.java
@@ -29,13 +29,4 @@ public class SearchForProductStory {
         endUser.should_see_results_with_the_given_product_name("iPhone");
     }
 
-
-//    @Test
-//    public void searching_for_unavailable_product_should_display_the_some_products() {
-//        endUser.is_the_home_page();
-//        endUser.looks_for("pear");
-//        endUser.should_see_definition("An edible fruit produced by the pear tree, similar to an apple but elongated towards the stem.");
-//    }
-
-
 } 
